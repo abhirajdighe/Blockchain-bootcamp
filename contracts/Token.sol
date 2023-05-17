db5ff29d128a81246ecbb68c95c88b8421c8f7d0
Abhiraj Dighe <digheabhirajr262@gmail.com>
2023-05-17 18:54:55 +0530

    Testing Contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,5 +4,14 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract Token{
-    string public name = "My Tokens";
+    string public name ;
+    string public symbol;
+    uint256 public decimals = 18;
+    uint public totalSupply = 1000000 * (10**decimals);
+
+    constructor(string memory _name,string memory _symbol,uint _totalSupply){
+        name= _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply*(10**decimals);
+    }
 }
---
+++ test/Token.js
@@ -0,0 +1,49 @@
+const {ethers} = require('hardhat')
+const {expect} = require('chai')
+
+describe("Token",()=>{
+    // Testes goes inside here : 
+
+    let token ;
+    const tokens = (n)=>{
+        return ethers.utils.parseUnits(n.toString(),'ether')
+    }
+    beforeEach(async ()=>{
+        // Fetch the token from blockchain : 
+        const Token = await ethers.getContractFactory("Token")
+        token = await Token.deploy('Dapp University','DAPP','1000000')
+    })
+
+    describe('Deployment',()=>{
+        const name = 'Dapp University';
+        const symbol = 'DAPP';
+        const decimal ='18';
+        const totalSupply=tokens('1000000');
+
+
+        it("has correct name",async ()=>{
+            // Check the name is correct : 
+            expect(await token.name()).to.equal(name)
+        })
+    
+        it("has correct symbol",async ()=>{
+            //Read token name : 
+           const symbol =  await token.symbol();
+            // Check the name is correct : 
+            expect(symbol).to.equal(symbol)
+        })
+    
+        it("has correct decimals",async ()=>{
+            // Check the name is correct : 
+            expect(await token.decimals()).to.equal(decimal)
+        })
+    
+        it("has correct totalSupply",async ()=>{
+            // Check the name is correct : 
+            expect(await token.totalSupply()).to.equal(totalSupply)
+        })
+    })
+    
+    
+
+})
