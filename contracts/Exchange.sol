3d333f551575094eb7bc3b703119e9000ff0686b
Abhiraj Dighe <digheabhirajr262@gmail.com>
2023-06-10 12:38:28 +0530

    updated make order

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+ // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
@@ -8,9 +8,35 @@ contract Exchange{
     address public feeAccount;
     uint public feePersent;
     mapping(address=>mapping(address=>uint)) public tokens;
+    mapping(uint => _Order) public orders;
+    uint public orderCount;
 
     event Deposit(address token,address user,uint amount,uint balance);
     event Withdraw(address token,address user,uint amount,uint balance);
+    
+    event Order(
+        // Attribute of an order :
+        uint id,
+        address user,
+        address tokenGet,
+        uint amountGet,
+        address tokenGive,
+        uint amountGive,
+        uint timestamp
+    );
+
+
+    // A way to model order
+    struct _Order{
+        // Attribute of an order :
+        uint id;
+        address user;
+        address tokenGet;
+        uint amountGet;
+        address tokenGive;
+        uint amountGive;
+        uint timestamp;
+    }
 
     constructor (address _feeAccount,uint _feePersent){
         feeAccount = _feeAccount;
@@ -44,4 +70,41 @@ contract Exchange{
     public view returns(uint){
         return tokens[_token][_user];
     }
+
+    // Make and cancel order :
+
+    function makeOrder(
+        address _tokenGet,
+        uint _amountGet,
+        address _tokenGive,
+        uint _amountGive
+    ) public {
+    // Token give (the token they want to spend) - which token and how much
+    // token get  (the token they want to receive) - which token and how much
+    require(balanceOf(_tokenGive,msg.sender)>=_amountGive);
+    
+    orderCount = orderCount+1;
+    orders[orderCount]=_Order(
+        orderCount,
+        msg.sender,
+        _tokenGet,
+        _amountGet,
+        _tokenGive,
+        _amountGive,
+        block.timestamp
+    );
+
+    // emit event :
+    emit Order(
+        orderCount,
+        msg.sender,
+        _tokenGet,
+        _amountGet,
+        _tokenGive,
+        _amountGive,
+        block.timestamp
+    );
+
+    }
+
 }
